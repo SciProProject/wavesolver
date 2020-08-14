88ab08fa0eb5ae05912133f07df325cfc7716eb8
fabian <fg.piwowarczyk@gmail.com>
2020-08-14 13:02:17 +0200

    bug fix in the code for visualizer

--- justtrying.py
+++ justtrying.py
@@ -1,12 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.linalg as sl
 
 
-x = np.linspace(-2, 2, 100)
 eigv = np.loadtxt('output/energies.dat')
 pot = np.loadtxt('output/potential.dat')
 wavef = np.loadtxt('output/wavefuncs.dat')
+
+for i, val in enumerate(eigv):
+    b = wavef[:, i+1] + val
+    wavef[:, i+1] = b
+
 
 eigval = np.empty((len(pot[:,0]), len(eigv)+1))
 eigval[:, 0] = pot[:, 0]
@@ -14,13 +17,19 @@ for i, val in enumerate(eigv):
     eigval[:, i+1] = val
 
 
-fig1, ax = plt.subplots()
+fig1, ax = plt.subplots(figsize = (4,6)) 
 
 plt.plot(pot[:, 0], pot[:, 1], color = 'black')
 
 ax.set_prop_cycle('color', ['blue', 'red'])
 for i in range(0,len(eigv)):
     plt.plot(eigval[:, 0], eigval[:,i+1], color = 'grey', alpha = 0.5)
-    #plt.plot(wavef[:, 0], wavef[:, val])
+    plt.plot(wavef[:, 0], wavef[:, i+1])
+    
+plt.xlabel('x [Bohr]')
+plt.ylabel('Energy [Hartree]')
+plt.title(r'Potential, eigenstates, $\langle x \rangle$')
 
-#/home/fabian/scipro/input/schrodinger.inp
+#/home/fabian/scipro/input/infpot.inp
+#/home/fabian/scipro/input/finitepot.inp
+#/home/fabian/scipro/input/cubspli.inp
