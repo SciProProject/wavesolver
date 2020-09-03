8466e2676877905ab8dca2bc86c6342665cc25e4
FabianPiwowarczyk <67393931+FabianPiwowarczyk@users.noreply.github.com>
2020-09-03 14:31:51 +0200

    introduced visualization expvalues

--- communicator.py
+++ communicator.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python3
 """Retrives data for the solvers module from the files given by the user."""
 import solvers
 import visualization
 
+def main():
+    filedir = input('Please input the directory of the file with the data. ')
 
-filedir = input('Please input the directory of the file with the data. ')
+    solvers.run(filedir)
 
-solvers.run(filedir)
+    visualization.run()
 
-visualization.run()
+if __name__ == '__main__':
+    main()
--- visualization.py
+++ visualization.py
@@ -4,10 +4,9 @@ import matplotlib.pyplot as plt
 
 
 def run():
+
     scaling = input("Do you want to scale your axes manually? [y/n] ")
-    
     if scaling == 'y':
-        
         scalingy = input("Do you want to scale the y-axis? [y/n] ")
         if scalingy == 'y':
             yMin = input("What do you want as lower end for the y-axis? ")
@@ -15,7 +14,9 @@ def run():
             yMax = input("What do you want as top end for the y-axis? ")
             yMax = float(yMax)
             pary = [yMin, yMax]
-            
+        else:
+            pary = None
+
         scalingx = input("Do you want to scale the x-axis? [y/n] ")
         if scalingx == 'y':
             xMin = input("What do you want as lower end for the x-axis? ")
@@ -23,43 +24,45 @@ def run():
             xMax = input("What do you want as top end for the x-axis? ")
             xMax = float(xMax)
             parx = [xMin, xMax]
-        
+        else:
+            parx = None
+
         visualization(pary, parx)
-    
+
     if scaling == 'n':
-        
         visualization()
-        
-    
 
 
 def visualization(pary=None, parx=None):
     eigv = np.loadtxt('output/energies.dat')
     pot = np.loadtxt('output/potential.dat')
     wavef = np.loadtxt('output/wavefuncs.dat')
-    
-    
+    #expv = np.loadtxt('output/expvalues.dat')
+
+
+    scal = 1 / (eigv[-1] + 1)
     for i, val in enumerate(eigv):
-        b = wavef[:, i+1] + val
+        b = wavef[:, i+1] * scal + val
         wavef[:, i+1] = b
 
 
     eigval = np.empty((len(pot[:,0]), len(eigv)+1))
     eigval[:, 0] = pot[:, 0]
-    
+
     for i, val in enumerate(eigv):
         eigval[:, i+1] = val
 
 
-    fig1, (ax1, ax2) = plt.subplots(1, 2, figsize = (8,6)) 
+    fig1, (ax1, ax2) = plt.subplots(1, 2, figsize=(8,6), sharey=True)
 
     ax1.plot(pot[:, 0], pot[:, 1], color = 'black')
 
     ax1.set_prop_cycle('color', ['blue', 'red'])
     for i in range(0,len(eigv)):
-        ax1.plot(eigval[:, 0], eigval[:,i+1], color = 'grey', alpha = 0.5)
+        ax1.plot(eigval[:, 0], eigval[:,i+1], color='grey', alpha=0.5)
         ax1.plot(wavef[:, 0], wavef[:, i+1])
-    
+        #ax1.plot(expv[i, 0], eigv[i, 0], color='green', marker='x')
+
     if pary:
         ax1.set_ylim(pary[0], pary[1])
     if parx:
@@ -68,10 +71,13 @@ def visualization(pary=None, parx=None):
     ax1.set_xlabel('x [Bohr]')
     ax1.set_ylabel('Energy [Hartree]')
     ax1.set_title(r'Potential, eigenstates, $\langle x \rangle$')
-    
-    
+
+
+    for i in range(0,len(eigv)):
+        ax2.plot(eigval[:, 0], eigval[:,i+1], color = 'grey', alpha = 0.5)
+        #ax2.plot(expv[i, 1], eigv[i, 0], color='magenta', marker='+')
+
+    ax2.set_xlabel('[Bohr]')
+    ax2.set_title('$\sigma_{x}$')
+
     plt.show()
-    
-#/home/fabian/scipro/input/infpot.inp
-#/home/fabian/scipro/input/finitepot.inp
-#/home/fabian/scipro/input/cubspli.inp
